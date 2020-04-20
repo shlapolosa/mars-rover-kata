4af6d76859057caffbed17721870677f452bb891
Hlapolosa <SocratesH@nedbank.co.za>
2020-04-20 14:22:25 +0200

    added java implementation skaffolding

---
+++ java/README.md
@@ -0,0 +1,4 @@
+Start in the Test Directory.
+
+To run;
+> mvn exec:java -Dexec.mainClass="com.tasks.thoughtworks.MarsRoverApplication" -Dexec.args="inputfile.txt"
---
+++ java/src/main/java/com/tasks/thoughtworks/MarsRoverApplication.java
@@ -0,0 +1,9 @@
+package com.tasks.thoughtworks;
+
+public class MarsRoverApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo rover world");
+    }
+
+}
