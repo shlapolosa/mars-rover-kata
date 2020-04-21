61dcef8e48b2920966b71f5b43edc2f76ed4453a
Socrates Hlapolosa <socrates.hlapolosa@gmail.com>
2020-04-21 12:06:24 +0200

    refactored exploration to return a list of elements rather than feature envy with a getter

--- java/src/main/java/com/tasks/thoughtworks/Exploration.java
+++ java/src/main/java/com/tasks/thoughtworks/Exploration.java
@@ -1,10 +1,6 @@
 package com.tasks.thoughtworks;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
@@ -13,23 +9,19 @@ public class Exploration {
 
     private Stream<String> inputCommands;
     private Plateau plateau;
+    private List<Object> explorationElements = new ArrayList<Object>();
 
     public Exploration(Stream<String> inputCommands) {
         this.inputCommands = inputCommands;
     }
 
-    public String start() {
+    public List<Object> start() {
         List<String> commandsStrings = inputCommands.collect(toList());
-        if(commandsStrings.get(0).isEmpty()){
-            return "";
+        if (commandsStrings.get(0).isEmpty()) {
+            return this.explorationElements;
         }
-        List<Plateau> plateauList = Stream.of(commandsStrings.get(0)).map(plateau -> new Plateau(plateau)).collect(toList());
-        List<String> StringList = commandsStrings.subList(1, commandsStrings.size());
-        this.plateau = plateauList.get(0);
-        return this.plateau.toString();
-    }
-
-    public Plateau getPlateau() {
-        return plateau;
+        this.plateau = Stream.of(commandsStrings.get(0)).map(plateau -> new Plateau(plateau)).collect(toList()).get(0);
+        explorationElements.add(plateau);
+        return this.explorationElements;
     }
 }
--- java/src/test/java/com/tasks/thoughtworks/ExplorationTest.java
+++ java/src/test/java/com/tasks/thoughtworks/ExplorationTest.java
@@ -3,7 +3,10 @@ package com.tasks.thoughtworks;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
 
 
 public class ExplorationTest {
@@ -12,30 +15,29 @@ public class ExplorationTest {
     public void should_return_no_updated_positions_if_an_empty_input_is_given() {
         Stream<String> stream = Stream.of("");
         Exploration exploration = new Exploration(stream);
-        Assert.assertEquals("",exploration.start());
+        Assert.assertTrue(exploration.start().isEmpty());
     }
 
     @Test
     public void should_read_and_return_the_dimensions_of_the_plateu_if_thats_all_thats_supplied() {
         Stream<String> stream = Stream.of("5 5");
         Exploration exploration = new Exploration(stream);
-        Assert.assertEquals("5 5",exploration.start());
+        Assert.assertEquals("5 5",exploration.start().get(0).toString());
     }
 
     @Test
     public void should_create_a_plateau_if_the_input_is_not_empty() {
-        Stream<String> stream = Stream.of("5 5");
+        Stream<String> stream = Stream.of("3 3");
         Exploration exploration = new Exploration(stream);
-        exploration.start();
-        Assert.assertNotNull(exploration.getPlateau());
+        Assert.assertEquals("3 3", exploration.start().get(0).toString());
     }
 
     @Test
     public void should_create_a_plateau_of_the_same_dimensions_as_the_input() {
         Stream<String> stream = Stream.of("5 5");
         Exploration exploration = new Exploration(stream);
-        exploration.start();
-        Assert.assertEquals(5, exploration.getPlateau().getLength());
-        Assert.assertEquals(5, exploration.getPlateau().getWidth());
+        List<Object> objectList = exploration.start();
+        Assert.assertThat(objectList.get(0), instanceOf(Plateau.class));
     }
+
 }
